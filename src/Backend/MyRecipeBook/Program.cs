fb8a1a6632483eedc93b6b5db607312b0b3b2578
Eduardo Lima <eduardoolimaprog@gmail.com>
2025-06-30 22:22:36 -0300

    ajustes para url e strins com vario espaços

--- src/Backend/MyRecipeBook/Program.cs
+++ src/Backend/MyRecipeBook/Program.cs
@@ -1,15 +1,15 @@
-using MyRecipeBook.Filters;
-using MyRecipeBook.Middleware;
 using MyRecipeBook.Application;
+using MyRecipeBook.Converters;
+using MyRecipeBook.Filters;
 using MyRecipeBook.Infrastructure;
-using MyRecipeBook.Application.UseCases.User.Register;
-using MyRecipeBook.Infrastructure.Migrations;
 using MyRecipeBook.Infrastructure.Extensions;
+using MyRecipeBook.Infrastructure.Migrations;
+using MyRecipeBook.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +19,7 @@ builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)))
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -13,6 +13,7 @@ namespace WebApi.Test.User.Register
 {
     public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
     {
+        private readonly string method = "user";
         private readonly HttpClient _httpClient;
         public RegisterUserTest(CustomWebApplicationFactory factory) 
         {
@@ -23,7 +24,7 @@ namespace WebApi.Test.User.Register
         public async Task Success()
         {
             var request = RequestRegisterUserJsonBuilder.Build();
-            var response = await _httpClient.PostAsJsonAsync("User", request);
+            var response = await _httpClient.PostAsJsonAsync(method, request);
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
             
@@ -46,7 +47,7 @@ namespace WebApi.Test.User.Register
 
             _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
 
-            var response = await _httpClient.PostAsJsonAsync("User", request);
+            var response = await _httpClient.PostAsJsonAsync(method, request);
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
